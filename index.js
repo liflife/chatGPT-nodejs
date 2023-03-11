5e115f8b3cb71c3ad8ece128650f5ec5bc253920
luoxf <luoxf@startimes.com.cn>
2023-03-11 11:28:11 +0800

    vue

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ router.get("/image", async (ctx, next) => {
     ctx.body = "<img src=\"" + url + "\"></>"
 });
 
-
+app.use(express.static(path.join(__dirname, './dist')))
 // 启用路由
 app.use(router.routes()).use(router.allowedMethods());
 
